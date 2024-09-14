36e05b678bfc5746431834037030eb6b49636a52
Zvimarmor <118083006+Zvimarmor@users.noreply.github.com>
2024-09-14 22:44:57 +0300

    [2419] [Time Beats: 5.55%] [Memory Beats: 94.44%] - LeetPush

---
+++ 2419-Longest-Subarray-With-Maximum-Bitwise-AND.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_val = max(nums)
+        length = len(nums)
+        answer = 0
+        current = 0
+        for i in range(length):
+            if nums[i] == max_val:
+                current+=1
+            else:
+                if current>answer:
+                    answer = current
+                current = 0
+        if current > answer:
+            answer = current
+
+        return answer
+
+        
