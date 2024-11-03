1c0fe33b488735b324fe0dd8a7760fc5eae318ad
Zvimarmor <118083006+Zvimarmor@users.noreply.github.com>
2024-11-03 10:39:32 +0200

    [1207] [Time Beats: 19.84%] [Memory Beats: 10.28%] - LeetPush

---
+++ 1207-Unique-Number-of-Occurrences.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        length = len(arr)
+        ansdict = dict()
+
+        for i in range(length):
+            if arr[i] in ansdict.keys():
+                ansdict[arr[i]] += 1
+            else:
+                ansdict[arr[i]] = 1
+
+        numsdict = dict()
+
+        for key in ansdict:
+            if ansdict[key] in numsdict.keys():
+                numsdict[ansdict[key]].append(key)
+            else:
+                numsdict[ansdict[key]]= [key]
+
+        for key in numsdict:
+            if len(numsdict[key]) >= 2:
+                return False
+
+        return True
+
+        
