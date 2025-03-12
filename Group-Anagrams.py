04db7b6452da077d74e6fba5040438762bdf51c0
Zvimarmor <118083006+Zvimarmor@users.noreply.github.com>
2025-03-12 14:27:34 +0200

    [49] [Time Beats: 67.24%] [Memory Beats: 41.62%] - LeetPush

---
+++ Group-Anagrams.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        \\\
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        \\\
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            key = tuple(sorted(word))
+            anagrams[key].append(word)
+
+        return list(anagrams.values())
+
+        return grouped
+ 
